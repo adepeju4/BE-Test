19559026ef017b34b7da81163f612d47e039fe5d
denniman <dennimanadams@gmail.com>
2021-07-23 10:21:53 +0100

    refactor test for register endpoint

--- src/_test_/user.test.js
+++ src/_test_/user.test.js
@@ -5,35 +5,15 @@ import app from '../server.js'
 const request = supertest(app)
 
 
-describe('POST /user', () => {
-    it('responds with')
-})
-
-test('post route', async () => {
-    const response = await request.post('/api/register')
-    expect(response.status).toBe(400)
-    expect(response.body.message).toBe('All fields must be provided')
-})
-
-
-
-describe('user test', async () => {
-    let mongo
-    beforeAll(async () => {
-        process.env.JWT = 'SOME SECRET'
-        mongo = new MongoMemoryServer()
-        const mongoURI = await mongo.getUri()
-
-        await mongoose.connect(DBURL, { useNewUrlParser: true, useUnifiedTopology: true})
+describe('POST /api/register', () => {
+    it('responds with json', async () => {
+        const response = await request.post('/api/register')
+        .send({email: 'testdev@dev.com', password: '1234567'})
+        .set('Accept', 'application/json')
+        expect(response.status).toBe(400)
+        expect(response.body.message).toBe('All fields must be provided')
     })
-
-    afterAll(async () => {
-        await mongo.stop()
-        await mongoose.connection.close()
-    })
-
-
-
-
 })
+
+
 
